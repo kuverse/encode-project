83b636a23ddab809517824acd533687a56e23155
Marioo <marioyoussef936@gmail.com>
2024-12-14 23:41:24 +0200

    added join game

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import * as ContractData from "../../hardhat/artifacts/contracts/Game.sol/Game.json";
-import CommitStage from "./_Components/CommitStage";
 import DeployStage from "./_Components/DeployStage";
 import { parseEther } from "viem";
 import { useAccount, useWalletClient } from "wagmi";
@@ -15,6 +13,7 @@ const RockPaperScissorsGame: React.FC = () => {
   const { data: walletClient } = useWalletClient();
   const [rivalAddress, setRivalAddress] = useState("");
   const [betAmount, setBetAmount] = useState("0.1");
+  const [inputAddress, setInputAddress] = useState<string>("");
   const Router = useRouter();
   const client = usePublicClient();
 
@@ -41,9 +40,33 @@ const RockPaperScissorsGame: React.FC = () => {
       alert("Failed to deploy contract.");
     }
   };
-  //{gameStage === "commit" && <CommitStage onCommit={commitMove} />}
+  const handleNavigate = () => {
+    if (inputAddress.trim() === "") {
+      alert("Please enter a valid address!");
+      return;
+    }
+    Router.push(`/Game/${inputAddress}`);
+  };
   return (
     <div>
+      <div className="max-w-md mx-auto p-5 bg-white rounded-lg shadow-md mt-6">
+        <h2 className="text-2xl font-bold text-black">Navigate to Game</h2>
+
+        <div className="">
+          <label className="block font-medium text-black">Enter Address:</label>
+          <input
+            type="text"
+            value={inputAddress}
+            onChange={e => setInputAddress(e.target.value)}
+            placeholder="0x1234..."
+            className="w-full p-2 border rounded text-white"
+          />
+        </div>
+
+        <button onClick={handleNavigate} className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+          Go to Game
+        </button>
+      </div>
       <DeployStage
         rivalAddress={rivalAddress}
         setRivalAddress={setRivalAddress}
